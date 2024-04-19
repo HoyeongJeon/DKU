a90cd007a98259f18e772d5b348d5afcdab4be5e
HoyeongJeon <hoyeong0302@gmail.com>
2024-04-19 17:00:56 +0900

    원형 큐 구현

--- 4-1/data-structure/queue/circularQ/cQueueS.c
+++ 4-1/data-structure/queue/circularQ/cQueueS.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "cQueueS.h"  
+#include "cQueueS.h"
 
-// Create a new circular queue
 QueueType* createQueue(void) {
     QueueType* cQ;
     cQ = (QueueType*)malloc(sizeof(QueueType));
     cQ->front = 0;
-    cQ->rear = 0;    
+    cQ->rear = 0;
     return cQ;
 }
 
-// Check if the circular queue is empty
 int isCQueueEmpty(QueueType* cQ) {
     if(cQ->front == cQ->rear) {
-        printf("Circular Queue is empty!\n");
+        printf(" Circular Queue is empty! ");
         return 1;
     } else {
         return 0;
     }
 }
 
-// Check if the circular queue is full
 int isCQueueFull(QueueType* cQ) {
-    if((cQ->rear + 1) % cQ_SIZE == cQ->front) {
-        printf("Circular Queue is full!\n");
+    if(((cQ->rear+1) % cQ_SIZE) == cQ->front) {
+        printf(" Circular Queue is full! ");
         return 1;
     } else {
         return 0;
     }
 }
-
 void enCQueue(QueueType* cQ, element item) {
     if(isCQueueFull(cQ)) {
         return;
     } else {
         cQ->rear = (cQ->rear + 1) % cQ_SIZE;
-        cQ->queue[cQ->rear] = item;
+        cQ->queue[cQ->rear] = item; 
+        return;
     }
 }
 
 element deCQueue(QueueType* cQ) {
+    element item;
     if(isCQueueEmpty(cQ)) {
         return "empty";
     } else {
@@ -57,16 +55,15 @@ element peekCQ(QueueType* cQ) {
     }
 }
 
-// 다시 보기 ?
 void printCQ(QueueType* cQ) {
     int i, first, last;
     first = (cQ->front + 1) % cQ_SIZE;
     last = (cQ->rear + 1) % cQ_SIZE;
-    printf("Circular Queue: [");
+    printf(" Circular Queue: [");
     i = first;
-    while (i!=last) {
+    while(i != last) {
         printf("%3c", cQ->queue[i]);
         i = (i + 1) % cQ_SIZE;
     }
-    printf(" ]\n");
+    printf(" ] ");
 }
